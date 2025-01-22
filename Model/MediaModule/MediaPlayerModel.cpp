aaf5ffbff32b7445f21e2bd3517232c44743786a
latrung124 <53443778+latrung124@users.noreply.github.com>
2025-01-22 23:58:12 +0700

    Implement media info properties

--- Model/MediaModule/MediaPlayerModel.cpp
+++ Model/MediaModule/MediaPlayerModel.cpp
@@ -23,3 +23,115 @@ void MediaPlayerModel::update()
     // Update media player model here
     qDebug() << "Update media player model";
 }
+
+QString MediaPlayerModel::title() const
+{
+    return m_title;
+}
+
+void MediaPlayerModel::setTitle(const QString &title)
+{
+    if (m_title != title)
+    {
+        m_title = title;
+        emit titleChanged();
+    }
+}
+
+QString MediaPlayerModel::artist() const
+{
+    return m_artist;
+}
+
+void MediaPlayerModel::setArtist(const QString &artist)
+{
+    if (m_artist != artist)
+    {
+        m_artist = artist;
+        emit artistChanged();
+    }
+}
+
+QString MediaPlayerModel::albumTitle() const
+{
+    return m_albumTitle;
+}
+
+void MediaPlayerModel::setAlbumTitle(const QString &albumTitle)
+{
+    if (m_albumTitle != albumTitle)
+    {
+        m_albumTitle = albumTitle;
+        emit albumTitleChanged();
+    }
+}
+
+QString MediaPlayerModel::albumArtist() const
+{
+    return m_albumArtist;
+}
+
+void MediaPlayerModel::setAlbumArtist(const QString &albumArtist)
+{
+    if (m_albumArtist != albumArtist)
+    {
+        m_albumArtist = albumArtist;
+        emit albumArtistChanged();
+    }
+}
+
+QString MediaPlayerModel::subtitle() const
+{
+    return m_subtitle;
+}
+
+void MediaPlayerModel::setSubtitle(const QString &subtitle)
+{
+    if (m_subtitle != subtitle)
+    {
+        m_subtitle = subtitle;
+        emit subtitleChanged();
+    }
+}
+
+QString MediaPlayerModel::genres() const
+{
+    return m_genres;
+}
+
+void MediaPlayerModel::setGenres(const QString &genres)
+{
+    if (m_genres != genres)
+    {
+        m_genres = genres;
+        emit genresChanged();
+    }
+}
+
+int MediaPlayerModel::totalTracks() const
+{
+    return m_totalTracks;
+}
+
+void MediaPlayerModel::setTotalTracks(int totalTracks)
+{
+    if (m_totalTracks != totalTracks)
+    {
+        m_totalTracks = totalTracks;
+        emit totalTracksChanged();
+    }
+}
+
+int MediaPlayerModel::trackNumber() const
+{
+    return m_trackNumber;
+}
+
+void MediaPlayerModel::setTrackNumber(int trackNumber)
+{
+    if (m_trackNumber != trackNumber)
+    {
+        m_trackNumber = trackNumber;
+        emit trackNumberChanged();
+    }
+}
--- Model/MediaModule/MediaPlayerModel.h
+++ Model/MediaModule/MediaPlayerModel.h
@@ -9,16 +9,68 @@
 #define MEDIA_PLAYER_MODEL_H
 
 #include <QObject>
+#include <QString>
 
 class MediaPlayerModel : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString title READ title WRITE setTitle NOTIFY titleChanged)
+    Q_PROPERTY(QString artist READ artist WRITE setArtist NOTIFY artistChanged)
+    Q_PROPERTY(QString albumTitle READ albumTitle WRITE setAlbumTitle NOTIFY albumTitleChanged)
+    Q_PROPERTY(QString albumArtist READ albumArtist WRITE setAlbumArtist NOTIFY albumArtistChanged)
+    Q_PROPERTY(QString subtitle READ subtitle WRITE setSubtitle NOTIFY subtitleChanged)
+    Q_PROPERTY(QString genres READ genres WRITE setGenres NOTIFY genresChanged)
+    Q_PROPERTY(int totalTracks READ totalTracks WRITE setTotalTracks NOTIFY totalTracksChanged)
+    Q_PROPERTY(int trackNumber READ trackNumber WRITE setTrackNumber NOTIFY trackNumberChanged)
 
 public:
     MediaPlayerModel(QObject *parent = nullptr);
     virtual ~MediaPlayerModel();
 
+    QString title() const;
+    void setTitle(const QString &title);
+
+    QString artist() const;
+    void setArtist(const QString &artist);
+
+    QString albumTitle() const;
+    void setAlbumTitle(const QString &albumTitle);
+
+    QString albumArtist() const;
+    void setAlbumArtist(const QString &albumArtist);
+
+    QString subtitle() const;
+    void setSubtitle(const QString &subtitle);
+    QString genres() const;
+    void setGenres(const QString &genres);
+
+    int totalTracks() const;
+    void setTotalTracks(int totalTracks);
+
+    int trackNumber() const;
+    void setTrackNumber(int trackNumber);
+
     void update();
+
+signals:
+    void titleChanged();
+    void artistChanged();
+    void albumTitleChanged();
+    void albumArtistChanged();
+    void subtitleChanged();
+    void genresChanged();
+    void totalTracksChanged();
+    void trackNumberChanged();
+
+private:
+    QString m_title;
+    QString m_artist;
+    QString m_albumTitle;
+    QString m_albumArtist;
+    QString m_subtitle;
+    QString m_genres;
+    int m_totalTracks;
+    int m_trackNumber;
 };
 
 #endif // MEDIA_PLAYER_MODEL_H
